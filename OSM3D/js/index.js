8c5a01b33acbde64d1c2bfc523f6e06e043052ef
CheAbdullah <chezulkhairi@gmail.com>
2018-01-08 22:13:26 +0000

    Add files via upload

---
+++ OSM3D/js/index.js
@@ -0,0 +1,145 @@
+var map = new L.Map('map');
+map.setView([52.52111, 13.40988], 16, false);
+
+new L.TileLayer('https://{s}.tiles.mapbox.com/v3/osmbuildings.kbpalbpk/{z}/{x}/{y}.png', {
+  attribution: '© Map <a href="https://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  maxNativeZoom: 20
+}).addTo(map);
+
+// Add layers to the map
+L.control.layers({
+  'Satellite Map': L.mapbox.tileLayer('bobbysud.map-l4i2m7nd', {
+      detectRetina: true
+  }).addTo(map),
+  'Terrain Map': L.mapbox.tileLayer('bobbysud.i2pfp2lb', {
+      detectRetina: true
+  })
+}).addTo(map);
+
+var osmb = new OSMBuildings(map).load();
+
+//********************************************************
+
+var
+  now,
+  date, time,
+  timeRange, dateRange,
+  timeRangeLabel, dateRangeLabel;
+
+function changeDate() {
+  var Y = now.getFullYear(),
+    M = now.getMonth(),
+    D = now.getDate(),
+    h = now.getHours(),
+    m = 0;
+
+  timeRangeLabel.innerText = pad(h) + ':' + pad(m);
+  dateRangeLabel.innerText = Y + '-' + pad(M+1) + '-' + pad(D);
+
+  osmb.date(new Date(Y, M, D, h, m));
+}
+
+function onTimeChange() {
+  now.setHours(this.value);
+  now.setMinutes(0);
+  changeDate();
+}
+
+function onDateChange() {
+  now.setMonth(0);
+  now.setDate(this.value);
+  changeDate();
+}
+
+function pad(v) {
+  return (v < 10 ? '0' : '') + v;
+}
+
+timeRange = document.getElementById('time');
+dateRange = document.getElementById('date');
+timeRangeLabel = document.querySelector('*[for=time]');
+dateRangeLabel = document.querySelector('*[for=date]');
+
+now = new Date;
+changeDate();
+
+// init with day of year
+var Jan1 = new Date(now.getFullYear(), 0, 1);
+dateRange.value = Math.ceil((now-Jan1)/86400000);
+
+timeRange.value = now.getHours();
+
+timeRange.addEventListener('change', onTimeChange);
+dateRange.addEventListener('change', onDateChange );
+timeRange.addEventListener('input', onTimeChange);
+dateRange.addEventListener('input', onDateChange);
+
+//************************************************************
+
+// load extra information
+function ajax(url, callback) {
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (req.readyState !== 4) {
+      return;
+    }
+    if (!req.status || req.status < 200 || req.status > 299) {
+      return;
+    }
+
+    callback(JSON.parse(req.responseText));
+  };
+  req.open('GET', url);
+  req.send(null);
+}
+
+// formatted json output
+function formatJSON(json) {
+  var html = '';
+  for (var key in json) {
+    html += '<em>'+ key +'</em> '+ json[key] +'<br>';
+  }
+  return html;
+}
+
+osmb.click(function(e) {
+  var url = 'https://data.osmbuildings.org/0.2/uejws863/feature/'+ e.feature +'.json';
+  ajax(url, function(json) {
+    var content = '<b>OSM ID '+ e.feature +'</b>';
+    for (var i = 0; i < json.features.length; i++) {
+      content += '<br><em>OSM Part ID</em> '+ json.features[i].id;
+      content += '<br>'+ formatJSON(json.features[i].properties.tags);
+    }
+
+    L.popup({ maxHeight:200, autoPanPaddingTopLeft:[50,50] })
+      .setLatLng(L.latLng(e.lat, e.lon))
+      .setContent(content)
+      .openOn(map);
+  });
+});
+
+var featureGroup = L.featureGroup().addTo(map);
+var drawControl = new L.Control.Draw({
+    edit: {
+        featureGroup: featureGroup
+    }
+}).addTo(map);
+map.on('draw:created', function(e) {
+    // Each time a feaute is created, it's added to the over arching feature group
+    featureGroup.addLayer(e.layer);
+});
+// on click, clear all layers
+document.getElementById('delete').onclick = function(e) {
+    featureGroup.clearLayers();
+}
+document.getElementById('export').onclick = function(e) {
+    // Extract GeoJson from featureGroup
+    var data = featureGroup.toGeoJSON();
+    // Stringify the GeoJson
+    var convertedData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
+    // Create export
+    document.getElementById('export').setAttribute('href', 'data:' + convertedData);
+    document.getElementById('export').setAttribute('download','data.geojson');
+}
+
