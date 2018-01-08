7d3ab7d84ec175cb70f5b5a1daafc04d991aab97
CheAbdullah <chezulkhairi@gmail.com>
2018-01-08 23:27:09 +0000

    Add files via upload

--- OSM3D3/js/index - Copy.js
+++ OSM3D3/js/index - Copy.js
@@ -1,52 +1,11 @@
-// var map = new L.Map('map');
-// map.setView([52.52111, 13.40988], 16, false);
+var map = new L.Map('map');
+map.setView([52.52111, 13.40988], 16, false);
 
-// var osm=new L.TileLayer('https://{s}.tiles.mapbox.com/v3/osmbuildings.kbpalbpk/{z}/{x}/{y}.png', {
-//   attribution: '© Map <a href="https://mapbox.com">Mapbox</a>',
-//   maxZoom: 18,
-//   maxNativeZoom: 20
-// });
-
-
-
-var cities = L.layerGroup();
-
-	L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.').addTo(cities),
-	L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.').addTo(cities),
-	L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(cities),
-	L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(cities);
-
-
-	var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		//mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
-    mbUrl='https://{s}.tiles.mapbox.com/v3/osmbuildings.kbpalbpk/{z}/{x}/{y}.png';
-	var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr}),
-		streets  = L.tileLayer(mbUrl, {id: 'mapbox.streets',   attribution: mbAttr});
-
-	// var map = L.map('map', {
-	// 	center: [39.73, -104.99],
-	// 	zoom: 10,
-	// 	layers: [grayscale, cities]
-  // });
-  
-    var map = new L.Map('map');
-    map.setView([52.52111, 13.40988], 16, false);
-
-	var baseLayers = {
-		"Grayscale": grayscale,
-		"Streets": streets
-	};
-
-	var overlays = {
-		"Cities": cities
-	};
-
-	L.control.layers(baseLayers, overlays).addTo(map);
-
-
-
+var osm=new L.TileLayer('https://{s}.tiles.mapbox.com/v3/osmbuildings.kbpalbpk/{z}/{x}/{y}.png', {
+  attribution: '© Map <a href="https://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  maxNativeZoom: 20
+});
 
 map.addLayer(osm);
 var osmb = new OSMBuildings(map).load();
