534e2d17f5c3e6645225fd20f4664e57fc888cb5
CheAbdullah <chezulkhairi@gmail.com>
2023-10-21 10:52:47 +0100

    Add files via upload

---
+++ PAL_MAP/js/constants.js
@@ -0,0 +1,8 @@
+const center = [34.4668,31.5017]; // Bengaluru
+const zoom = 12;
+
+// Before Map URL
+const map1URL = 'https://{s}.tiles.mapbox.com/v3/spatial.map-qgihrqg5/{z}/{x}/{y}.png';
+
+// After Map URL
+const map2URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
