e782e45caed0c2143253443f4b080a6ea412e93d
CheAbdullah <chezulkhairi@gmail.com>
2023-10-21 11:07:46 +0100

    Update constants.js

--- PAL_MAP/js/constants.js
+++ PAL_MAP/js/constants.js
@@ -2,7 +2,7 @@ const center = [34.4668,31.5017]; // Bengaluru
 const zoom = 5;
 
 // Before Map URL
-const map1URL = 'https://{s}.tiles.mapbox.com/v3/spatial.map-qgihrqg5/{z}/{x}/{y}.png';
+const map1URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 // After Map URL
 const map2URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
